cd048e3bfe4818e4db1b76ef70390ba5f6aee2f7
Daniel Nascimento <dbnkocao@gmail.com>
2020-03-23 19:16:54 -0300

    challenge #1; Withdraw mirror port on host to redis in docker-compose production file

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,29 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #create" do
-    it "returns http success" do
-      pending
-      get :create
-      expect(response).to have_http_status(:success)
-    end
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      pending
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    before(:each) do
+      @campaign = create(:campaign, user: @current_user)
+
+      @member_attributes = attributes_for(:member)
+      @member_attributes[:campaign_id] = @campaign.id
+      post :create, params: {member: @member_attributes}, format: :json
     end
+
+    it "Responsde should be success" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "Create meber with right attributes" do
+      expect(Member.last.campaign).to eql(@campaign)
+      expect(Member.last.name).to eql(@member_attributes[:name])
+      expect(Member.last.email).to eql(@member_attributes[:email])
+    end
+
+    # it "Create campaign with owner associated as a member" do
+    #   expect(Campaign.last.members.last.name).to eql(@current_user.name)
+    #   expect(Campaign.last.members.last.email).to eql(@current_user.email)
+    # end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      pending
-      get :update
-      expect(response).to have_http_status(:success)
-    end
+  describe "PUT #update" do
+    pending
   end
 
+  describe "DELETE #destroy" do
+    before(:each) do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    context "User is the Campaign Owner" do
+      it "returns http success" do
+        campaign = create(:campaign, user: @current_user)
+        member = create(:member, campaign: campaign)
+        delete :destroy, params: {id: member.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "User isn't the Campaign Owner" do
+      it "returns http forbidden" do
+        member = create(:member)
+        delete :destroy, params: {id: member.id}
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
 end
